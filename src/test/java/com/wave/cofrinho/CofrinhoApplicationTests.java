4fd5171470718353e1db495be71c633a4f4119c3
edson <edson-456@hotmail.com>
2024-08-10 17:42:50 -0300

    primeiro commit

---
+++ src/test/java/com/wave/cofrinho/CofrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.cofrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
